06a99cc6cfc6d4ff86be4d05162acb181dc0853f
Dominic <jxdlam@gmail.com>
2016-08-01 14:00:01 +0800

    added seed

---
+++ seed.js
@@ -0,0 +1,24 @@
+var seeder = require('mongoose-seed')
+
+seeder.connect(process.env.MONGODB_URI, function () {
+  seeder.loadModels([
+    'models/profile.js'
+  ])
+
+  seeder.clearModels(['Profile'], function () {
+    seeder.populateModels(data, function () {
+      console.log('its working!')
+    })
+  })
+})
+
+var data = [{
+  'model': 'Profile',
+  'documents': [{
+    name: 'Dominic Lam',
+    tagline: 'This is a tagline',
+    about: 'I am trying out something <br /> not sure if this works',
+    contact: '1234',
+    github: '1234'
+  }]
+}]
