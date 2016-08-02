b27a35beb3f814fc7336e066328f1168663e4293
Dominic <jxdlam@gmail.com>
2016-08-02 15:28:31 +0800

    begun front end; init angular and created index page

--- public/core.js
+++ public/core.js
@@ -1,11 +1,1 @@
-
-$.ajax({
-  url: 'http://localhost:3000/profile',
-  method: 'GET',
-  dataType: 'json',
-  success: function (profile) {
-    $.each(profile, function (index) {
-      $('h2').append(profile[index].about)
-    })
-  }
-})
+angular.module('myApp', ['ngRoute'])
---
+++ public/profile/profile.js
@@ -0,0 +1,19 @@
+angular.module('myApp')
+  .filter('unsafe', function ($sce) {
+    return function (val) {
+      return $sce.trustAsHtml(val)
+    }
+  })
+  .component('profilePage', {
+    templateUrl: 'profile.template.html',
+    controller: function ($scope, $http) {
+      $http({
+        method: 'GET',
+        url: 'http://localhost:3000/api/profile'
+      }).then(function success (res) {
+        $scope.myInfo = res.data
+      }, function error (err) {
+        console.log(err)
+      })
+    }
+  })
